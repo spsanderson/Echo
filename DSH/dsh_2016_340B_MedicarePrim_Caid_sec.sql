743f1b3212670e9c197cbf1d8d213a411b44462a
Steven Sanderson <spsanderson@gmail.com>
2019-03-18 22:15:00 -0400

    fix duplication issue

--- DSH/dsh_2016_340B_MedicarePrim_Caid_sec.sql
+++ DSH/dsh_2016_340B_MedicarePrim_Caid_sec.sql
@@ -1,29 +1,28 @@
-SELECT A.[PA-PT-NO-WOSCD],
-	A.[PA-PT-NO-SCD],
-	A.[PT-NO],
-	A.[PA-UNIT-NO],
-	B.[admit_date],
-	C.[PA-DTL-REV-CD],
-	C.[Adjusted Rev Code],
-	C.[Rev Code from CDM],
-	A.[PA-DTL-SVC-CD],
-	[PA-DTL-UNIT-DATE],
-	A.[TYPE],
-	[340B-IND],
-	[REPORTING-GROUP],
-	[TOT-CHG-QTY],
-	[Total Charges including Prof Fees]
-FROM [DSH].[dbo].[2016_340B_INDICATOR] a
-LEFT JOIN [Encounters_For_DSH] b ON a.[pa-pt-no-woscd] = b.[pa-pt-no-woscd]
-	--AND A.[PA-DTL-DATE] >= B.[START_UNIT_DATE]
-	--AND A.[PA-DTL-DATE] <= B.[END_UNIT_DATE]
-	--AND a.[pa-ctl-paa-xfer-date] = b.[pa-ctl-paa-xfer-date]
-	--AND A.[PA-UNIT-DATE] = B.[pa-unit-date]
-	AND A.[pa-unit-no] = B.[pa-unit-no]
---DATEADD(DAY,-(DAY(DATEADD(MONTH, 1,a.[pa-dtl-date]))),DATEADD(MONTH,1,a.[pa-dtl-date]))
-LEFT JOIN [dbo].[2016_DSH_Costs] C ON a.[pa-pt-no-woscd] = b.[pa-pt-no-woscd]
-	AND A.[PA-PT-NO-SCD] = B.[PA-PT-NO-SCD]
-	AND A.[PA-DTL-SVC-CD] = C.[PA-DTL-SVC-CD]
-WHERE [REPORTING-GROUP] IN ('PRIMARY MEDICAID', 'PRIMARY MEDICAID MANAGED CARE', 'MEDICAID FFS DUAL ELIGIBLE', 'MEDICAID MANAGED CARE DUAL ELIGIBLE', 'PRIMARY OUT OF STATE MEDICAID', 'DUAL ELIGIBLE OUT OF STATE MEDICAID')
-	--AND [PA-PT-NO-WOSCD] = '1010716363'
-	--order by [admit_date] desc
+SELECT COSTS.[pa-pt-no-woscd],
+	COSTS.[PA-PT-NO-SCD],
+	CAST(COSTS.[pa-pt-no-woscd] AS VARCHAR) + CAST(COSTS.[PA-PT-NO-SCD] AS VARCHAR) AS [PT-NO],
+	COSTS.[PA-UNIT-NO],
+	ENC.admit_date,
+	COSTS.[PA-DTL-REV-CD],
+	COSTS.[Adjusted Rev Code],
+	COSTS.[Rev Code from CDM],
+	B340.[PA-DTL-SVC-CD],
+	B340.[PA-DTL-UNIT-DATE],
+	ENC.[ptacct_type],
+	B340.[340B-IND],
+	COSTS.[REPORTING GROUP],
+	B340.[TOT-CHG-QTY],
+	B340.[Total Charges including Prof Fees],
+	COSTS.[SUM OF TOT PROF FEES],
+	COSTS.[SUM OF TOT CHG]
+FROM [dbo].[2016_DSH_Costs] AS COSTS
+LEFT OUTER JOIN [DBO].[2016_340B_INDICATOR] AS B340 ON COSTS.[pa-pt-no-woscd] = B340.[PA-PT-NO-WOSCD]
+	AND COSTS.[PA-PT-NO-SCD] = B340.[PA-PT-NO-SCD]
+	AND COSTS.[PA-UNIT-NO] = B340.[PA-UNIT-NO]
+	AND COSTS.[PA-DTL-SVC-CD] = B340.[PA-DTL-SVC-CD]
+LEFT OUTER JOIN [dbo].[Encounters_For_DSH] AS ENC ON COSTS.[pa-pt-no-woscd] = ENC.[PA-PT-NO-WOSCD]
+	AND COSTS.[PA-PT-NO-SCD] = ENC.[PA-PT-NO-SCD]
+	AND COSTS.[PA-UNIT-NO] = ENC.[pa-unit-no]
+	--WHERE COSTS.[PA-PT-NO-WOSCD] = '1008416365'
+	--AND COSTS.[PA-PT-NO-SCD] = '7'
+	--;
