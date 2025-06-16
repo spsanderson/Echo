16f877a6d7ae2e7002336728d8013cdbedd41332
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2025-06-16 12:30:34 -0400

    Add files via upload

---
+++ R_Code/Payer_Policy/anthem_prefixes.R
@@ -0,0 +1,104 @@
+
+# Library Load ------------------------------------------------------------
+
+
+library(dplyr)
+library(rvest)
+library(purrr)
+library(tidyr)
+library(writexl)
+library(DBI)
+library(odbc)
+
+# Source SQL Connection Fns -----------------------------------------------
+
+source("W:/PATACCT/BusinessOfc/Revenue Cycle Analyst/R_Code/DSS_Connection_Functions.r")
+
+# Base URL for Prefixes ---------------------------------------------------
+
+# base_url
+base_url <- "https://mypayerdirectory.com/bcbs-prefix-list/"
+
+# xpath_location
+xpath_location <- '//*[@id="article"]'
+
+# read in the page
+page <- read_html(base_url)
+
+# generate a tibble of prefix urls to read
+prefix_urls_tbl <- html_node(page, xpath = xpath_location) |>
+  html_elements("a") |>
+  html_attr("href") |>
+  as_tibble() |>
+  set_names("link")
+
+# read in the page of each prefix url link
+page_tbl <- prefix_urls_tbl |>
+  mutate(
+    page_data = map(link, read_html)
+  )
+
+# Get a table of the prefixes from their links into a tibble column
+page_tbl <- page_tbl |>
+  mutate(
+    table_data = map(page_data, ~ html_table(.x))
+  )
+
+page_tbl <- page_tbl |>
+  mutate(
+    table_data = map(table_data,~ .x[[1]] |> 
+                       select(1, 2) |>
+                       set_names(c("plan_identifier_prefix", "plan_name"))
+    )
+  ) |>
+  unnest(cols = c(table_data))
+
+# Write to Share Drive ----------------------------------------------------
+
+
+base_path <- "W:/PATACCT/BusinessOfc/Revenue Cycle Analyst/Payer_Policies/"
+prefix_folder <- "anthem_prefixes"
+
+# Create the folder if it doesn't exist
+if (!dir.exists(file.path(base_path, prefix_folder))) {
+  dir.create(file.path(base_path, prefix_folder))
+}
+
+# Write the prefix table to an excel file. Only use the columns: 
+# - link
+# - plan_identifier_prefix
+# - plan_name
+# Then write the file to the share drive
+write_xlsx(
+  page_tbl |>
+    select(link, plan_identifier_prefix, plan_name),
+  file.path(base_path, prefix_folder, "anthem_prefixes.xlsx")
+)
+
+# Write data to SQL -------------------------------------------------------
+
+
+# Create a table if not exists called: c_anthem_prefixes_tbl
+con_obj <- db_connect()
+
+dbWriteTable(
+  conn = con_obj,
+  Id(
+    schema = "dbo",
+    table = "c_anthem_prefixes_tbl"
+  ),
+  page_tbl |>
+    select(plan_identifier_prefix, plan_name),
+  overwrite = TRUE,
+  row.names = FALSE
+)
+
+# Query table and count the rows just inserted into c_anthme_prefixes_tbl
+rows_inserted <- dbGetQuery(con_obj, "SELECT COUNT(*) AS row_count FROM dbo.c_anthem_prefixes_tbl")[[1]]
+
+db_disconnect(con_obj)
+
+# Message how many rows were just pushed to the table
+cat("Rows inserted into c_anthem_prefixes_tbl:", rows_inserted, "\n")
+
+# End of Script -----------------------------------------------------------
