639eef1a3939d75bee3b57cbda6511fe53d40d04
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2026-07-24 10:35:24 -0400

    Refactor R script for data processing and output
    remove the line:
    ```r
    rename(INS_CD = Ins_CD)
    ```

--- R_Code/combined_carc_rarc_distribution/combined_carc_rarc_distribution.R
+++ R_Code/combined_carc_rarc_distribution/combined_carc_rarc_distribution.R
@@ -11,7 +11,6 @@ head(df_tbl)
 
 ## Select out the needed columns ----
 df_modified_tbl <- df_tbl |>
-  rename(INS_CD = Ins_CD) |>
   # Drop records where the PCN == 0
   filter(Patient_Control_Number != 0)
 
