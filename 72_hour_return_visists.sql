f37d808151014c40ad48cc1b88c8c70dfe67659d
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2026-08-07 10:28:15 -0400

    Add files via upload

--- 72_hour_return_visists.sql
+++ 72_hour_return_visists.sql
@@ -1,28 +1,44 @@
-with cte as (
-	select mrn,
-		cast(Admit_Date as date) as [adm_date],
-		cast(Dsch_Date as date) as [dsch_date],
+WITH cte
+AS (
+	SELECT mrn,
+		cast(Admit_Date AS DATE) AS [adm_date],
+		cast(Dsch_Date AS DATE) AS [dsch_date],
 		pt_no,
 		Acct_Type,
 		hosp_svc,
 		hosp_svc_description,
-		[hours_until_next_visit] = datediff(hour, Dsch_Date, lead(admit_date) over(partition by mrn order by admit_date)),
-		[next_admit_date] = cast(lead(admit_date) over(partition by mrn order by admit_date) as date),
-		[next_dsch_date] = cast(lead(dsch_date) over(partition by mrn order by admit_date) as date),
-		[next_visit_no] = lead(pt_no) over(partition by mrn order by admit_date),
-		[next_acct_type] = lead(acct_type) over(partition by mrn order by admit_date),
-		[next_hosp_svc] = lead(hosp_svc) over(partition by mrn order by admit_date),
-		[next_hosp_svc_description] = lead(hosp_svc_description) over(partition by mrn order by admit_date)
-	from sms.dbo.Pt_Accounting_Reporting_ALT
-	where mrn != ''
-	and mrn is not null
-	and Unit_No is null
-	and Active_Archive = 'active'
-	and Tot_Chgs > 0
-	and FC != 'x'
-) 
+		[hours_until_next_visit] = datediff(hour, Dsch_Date, lead(admit_date) OVER (
+				PARTITION BY mrn ORDER BY admit_date
+				)),
+		[next_admit_date] = cast(lead(admit_date) OVER (
+				PARTITION BY mrn ORDER BY admit_date
+				) AS DATE),
+		[next_dsch_date] = cast(lead(dsch_date) OVER (
+				PARTITION BY mrn ORDER BY admit_date
+				) AS DATE),
+		[next_visit_no] = lead(pt_no) OVER (
+			PARTITION BY mrn ORDER BY admit_date
+			),
+		[next_acct_type] = lead(acct_type) OVER (
+			PARTITION BY mrn ORDER BY admit_date
+			),
+		[next_hosp_svc] = lead(hosp_svc) OVER (
+			PARTITION BY mrn ORDER BY admit_date
+			),
+		[next_hosp_svc_description] = lead(hosp_svc_description) OVER (
+			PARTITION BY mrn ORDER BY admit_date
+			)
+	FROM sms.dbo.Pt_Accounting_Reporting_ALT
+	WHERE mrn != ''
+		AND mrn IS NOT NULL
+		AND UNIT_NO IS NULL
+		AND Active_Archive = 'active'
+		AND Tot_Chgs > 0
+		AND FC != 'x'
+	)
+SELECT *
+FROM cte
+WHERE hours_until_next_visit <= 72
+	AND hours_until_next_visit >= 0
+	AND adm_date >= '2024-01-01'
 
-select *
-from cte
-where hours_until_next_visit <= 72
-and adm_date >= '2024-01-01'
